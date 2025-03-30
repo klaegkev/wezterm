e860c621817328cd832d43d7ffde3cea70a20c21
klaegkev <kevin.klaeger@proton.me>
2025-03-30 11:49:39 +0200

    update linux

--- wezterm.lua
+++ wezterm.lua
@@ -10,8 +10,14 @@ config.window_padding = {
 	bottom = 0,
 }
 
+config.window_decorations = "TITLE|RESIZE"
 config.window_background_opacity = 0.8
 config.macos_window_background_blur = 30
+
+-- Windows
 config.win32_system_backdrop = "Acrylic"
+
+-- Linux https://github.com/wezterm/wezterm/issues/4962
+config.enable_wayland = false
 
 return config
