f018b42f139f60402a46234a88371f791d99796e
klaegkev <kevin.klaeger@proton.me>
2025-04-02 21:36:25 +0200

    wezterm use powershell under windows

--- wezterm.lua
+++ wezterm.lua
@@ -16,6 +16,10 @@ config.window_decorations = "TITLE|RESIZE"
 config.macos_window_background_blur = 30
 
 -- Windows
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+	config.default_prog =
+		{ "powershell", "-NoExit", "-Command", "starship init powershell | out-string | Invoke-Expression" }
+end
 config.win32_system_backdrop = "Acrylic"
 
 -- Linux https://github.com/wezterm/wezterm/issues/4962
