3ff0b9a6cc1a788fc5af7a31b201677b39ac2968
klaegkev <kevin.klaeger@proton.me>
2025-04-10 20:01:17 +0200

    update font

--- wezterm.lua
+++ wezterm.lua
@@ -2,6 +2,8 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
+config.font = wezterm.font("Hack Nerd Font")
+config.font_size = 16
 config.color_scheme = "Catppuccin Mocha"
 config.window_decorations = "TITLE|RESIZE"
 config.enable_tab_bar = false
